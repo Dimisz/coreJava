736802fbc4ba52e9ab0b4cee9c97697f4bcd2901
Vladimir Solovyov <leilyngale@icloud.com>
2022-10-07 16:12:21 +0300

    Setup folder structure

---
+++ zaurBasics/BasicDataTypes/src/DataTypes.java
@@ -0,0 +1,7 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        System.out.println("Hi there!");
+        System.out.print("My name is ");
+        System.out.print("Vladimir Solovyov");
+    }
+}
